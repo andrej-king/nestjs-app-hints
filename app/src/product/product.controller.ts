c7efe20f4c8cc1a0147a43a5d744dc8be7946731
Andrej Pedchenko <a.pedchenko@sauleit.com>
2022-02-09 10:59:59 +0200

    Fixed code style by linter

--- app/src/product/product.controller.ts
+++ app/src/product/product.controller.ts
@@ -1,33 +1,31 @@
-import {Body, Controller, Delete, Get, HttpCode, Param, Patch, Post} from '@nestjs/common';
-import {ProductModel} from "./product.model";
-import {FindProductDto} from "./dto/find-product.dto";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common'
+import {ProductModel} from './product.model'
+import {FindProductDto} from './dto/find-product.dto'
 
 @Controller('product')
 export class ProductController {
-
-  @Post('create')
-  async create(@Body() dto: Omit<ProductModel, '_id'>) { // dto exclude _id field
-
-  }
+  @Post('create') // dto exclude _id field
+  async create(@Body() dto: Omit<ProductModel, '_id'>) {}
 
   @Get(':id')
-  async get(@Param('id') id: string) {
-
-  }
+  async get(@Param('id') id: string) {}
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {
-
-  }
+  async delete(@Param('id') id: string) {}
 
   @Patch(':id')
-  async patch(@Param('id') id: string, @Body() dto: ProductModel) {
-
-  }
+  async patch(@Param('id') id: string, @Body() dto: ProductModel) {}
 
   @HttpCode(200)
   @Post()
-  async find(@Body() dto: FindProductDto) {
-
-  }
+  async find(@Body() dto: FindProductDto) {}
 }
