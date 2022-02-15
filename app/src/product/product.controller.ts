231376795711c8f08fa8267b69843ac63327c747
Andrej Pedchenko <graduadu@gmail.com>
2022-02-15 22:51:12 +0200

    Added validator for find product dto. Added product constants. Realized product controllers. Added service methods for work with db. Added aggregate with mongo db, for custom change response from database.

--- app/src/product/product.controller.ts
+++ app/src/product/product.controller.ts
@@ -3,33 +3,64 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
+  HttpCode, NotFoundException,
   Param,
   Patch,
-  Post,
+  Post, UsePipes, ValidationPipe,
 } from '@nestjs/common'
 import {ProductModel} from './product.model'
 import {FindProductDto} from './dto/find-product.dto'
 import {CreateProductDto} from './dto/create-product.dto'
+import {ProductService} from './product.service'
+import {PRODUCT_NOT_FOUND} from './product.constants'
+import {BeAnObject, DocumentType} from '@typegoose/typegoose/lib/types'
 
 @Controller('product')
 export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
   @Post('create') // dto exclude _id field
-  async create(@Body() dto: CreateProductDto): Promise<void> {}
+  async create(@Body() dto: CreateProductDto): Promise<ProductModel> {
+    return this.productService.create(dto)
+  }
 
   @Get(':id')
-  async get(@Param('id') id: string): Promise<void> {}
+  async get(@Param('id') id: string) {
+    const product = this.productService.findById(id)
+    if (!product) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND)
+    }
+
+    return product
+  }
 
   @Delete(':id')
-  async delete(@Param('id') id: string): Promise<void> {}
+  async delete(@Param('id') id: string): Promise<void> {
+    const deletedProduct = this.productService.deleteById(id)
+    if (!deletedProduct) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND)
+    }
+  }
 
   @Patch(':id')
   async patch(
     @Param('id') id: string,
-    @Body() dto: ProductModel,
-  ): Promise<void> {}
+    @Body() dto: CreateProductDto,
+  ) {
+    const updatedProduct = this.productService.updateById(id, dto)
+    if (!updatedProduct) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND)
+    }
 
+    return updatedProduct
+  }
+
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
-  @Post()
-  async find(@Body() dto: FindProductDto): Promise<void> {}
+  @Post('find')
+  async find(@Body() dto: FindProductDto) {
+    const res = this.productService.findWithReviews(dto)
+    console.log(res)
+    return res
+  }
 }
