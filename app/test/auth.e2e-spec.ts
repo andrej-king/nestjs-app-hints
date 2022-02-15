b8fde31cab0a2606087b305737d663e89dcdde7c
Andrej Pedchenko <graduadu@gmail.com>
2022-02-15 21:17:30 +0200

    Added e2e tests for login

---
+++ app/test/auth.e2e-spec.ts
@@ -0,0 +1,78 @@
+import {Test, TestingModule} from '@nestjs/testing'
+import {INestApplication} from '@nestjs/common'
+import * as request from 'supertest'
+import {AppModule} from '../src/app.module'
+import {disconnect} from 'mongoose'
+import {AuthDto} from '../src/auth/dto/auth.dto'
+import {USER_NOT_FOUND_ERROR, WRONG_PASSWORD_ERROR} from '../src/auth/auth.constants'
+
+const loginDto: AuthDto = {
+  login: 'review-e2e@test.app',
+  password: 'review-password',
+}
+
+describe('AuthController (e2e)', () => {
+  let app: INestApplication
+  let createdId: string
+  let token: string
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+    await app.init()
+
+    // create user
+    await request(app.getHttpServer())
+      .post('/auth/register')
+      .send(loginDto)
+  })
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await disconnect()
+    await app.close()
+  })
+
+  it('/auth/login (POST) - success', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(loginDto)
+      .expect(200)
+      .then(({body}: request.Response) => {
+        expect(body.access_token).toBeDefined()
+      })
+  })
+
+  it('/auth/login (POST) - wrong password', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({...loginDto, password: '2'})
+      .expect(401)
+      .then(({body}: request.Response) => {
+        expect(body.statusCode).toBe(401)
+        expect(body.message).toBe(WRONG_PASSWORD_ERROR)
+      })
+  })
+
+  it('/auth/login (POST) - wrong login', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({...loginDto, login: 'test@app.test'})
+      .expect(401)
+      .then(({body}: request.Response) => {
+        expect(body.statusCode).toBe(401)
+        expect(body.message).toBe(USER_NOT_FOUND_ERROR)
+      })
+  })
+})
