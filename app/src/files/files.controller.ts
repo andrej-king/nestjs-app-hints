e4119b4e92d0e0116872d2fe0eab96d46923e38e
Andrej Pedchenko <graduadu@gmail.com>
2022-02-18 22:59:42 +0200

    Added module, controller, service for work with files. Installed types for multer (work with images). Installed fs-extra for simplify work with os. Installed app-root-path to get project root. Installed date-fns to work with date.

--- app/src/files/files.controller.ts
+++ app/src/files/files.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {Controller, HttpCode, Post, UploadedFile, UseGuards, UseInterceptors} from '@nestjs/common'
+import {FileInterceptor} from '@nestjs/platform-express'
+import {JwtAuthGuard} from '../auth/guards/jwt.guard'
+import {FileElementResponse} from './dto/file-element.response'
+import {FilesService} from './files.service'
 
 @Controller('files')
-export class FilesController {}
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {
+  }
+
+  @HttpCode(200)
+  @Post('upload')
+  // @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('files'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
+    return this.filesService.saveFiles([file])
+  }
+}
