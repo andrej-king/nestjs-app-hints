196e7a0bf2f1bbb8df797ae5c40379c345e0481a
Andrej Pedchenko <graduadu@gmail.com>
2022-02-20 22:02:06 +0200

    Added nestjs axios. Service for work with response from hh api

---
+++ app/src/hh/hh.constants.ts
@@ -0,0 +1,9 @@
+const ROOT_API = 'https://api.hh.ru'
+
+export const API_URL = {
+  vacancies: ROOT_API + '/vacancies'
+}
+
+export const SALARY_CLUSTER_ID = 'salary'
+
+export const NOT_FOUND_CLUSTER = 'Not found cluster salary'
--- app/src/hh/hh.service.ts
+++ app/src/hh/hh.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common'
+import {ConfigService} from '@nestjs/config'
+import {HttpService} from '@nestjs/axios'
+import {API_URL, NOT_FOUND_CLUSTER, SALARY_CLUSTER_ID} from './hh.constants'
+import {HhResponse} from './hh.models'
+import {lastValueFrom} from 'rxjs'
+import {HhData} from '../top-page/top-page.model'
 
 @Injectable()
-export class HhService {}
+export class HhService {
+  token: string
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {
+    this.token = this.configService.get('HH_TOKEN') ?? ''
+  }
+
+  async getData(text: string) {
+    try {
+      const httpResponse = this.httpService.get<HhResponse>(API_URL.vacancies, {
+        params: {
+          text,
+          cluster: true,
+        },
+        headers: {
+          'User-Agent': 'OwlTop/1.0 (app@example.com)',
+          Authorization: 'Bearer ' + this.token,
+        },
+      })
+      const {data} = await lastValueFrom(httpResponse)
+    } catch (e) {
+      Logger.error(e)
+    }
+  }
+
+  private parseData(data: HhResponse): HhData {
+    const salaryCluster = data.clusters.find(c => c.id == SALARY_CLUSTER_ID)
+    if (!salaryCluster) {
+      throw new Error(NOT_FOUND_CLUSTER)
+    }
+
+    const juniorSalary = 0
+    const middleSalary = 0
+    const seniorSalary = 0
+
+    return {
+      count: data.found,
+      juniorSalary,
+      middleSalary,
+      seniorSalary,
+      updatedAt: new Date()
+    }
+  }
+}
